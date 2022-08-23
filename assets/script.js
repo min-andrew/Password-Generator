afacdd7fa267a1cb85ac3570fd291d25479222f9
Andrew Min <58195643+min-andrew@users.noreply.github.com>
2022-08-22 21:02:34 -0700

    worked on understanding rng

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,16 @@
 // Assignment code here
+// 
+ const random = (length = 8) => {
+
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var password = "";
+  for (i = 0; i < length; i++) {
+   password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return password;
+};
+console.log(random());
 
 
 // Get references to the #generate element
