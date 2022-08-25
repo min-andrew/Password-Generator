533f978ebdd9b7eb2a0cf667df6764f42cb6da1f
Andrew Min <58195643+min-andrew@users.noreply.github.com>
2022-08-24 22:33:14 -0700

    worked on length prompt

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,30 @@
 //
 
 var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var length = 8
 
 function generatePassword() {
+  var length = prompt("how long do you want your password? (8-128)")
+  while (true) {
+    if (length === null) {
+      break
+    // } else if (length !== number) {
+    //   length = prompt("That is not a number. Please give a length between 8-128")
+    } else if (length <= 7) {
+      length = prompt("That is too short. Please give a length between 8-128")
+    } else if (length >= 128) {
+      length = prompt("That is too long. Please give a length between 8-128")
+    } else if (length >=8 && length <= 128){
+      break
+    }
+  };
+// if you cancel out of the prompt, you dont want these to continue. Make a if statment for that
+  var upper = confirm("Do you want uppercase characters?");
+  var lower = confirm("Do you want lowercase characters?");
+  var special = confirm("Do you want special characters?");
+  var num = confirm("Do you want numbers?");
+
+
+
   var password = "";
   for (i = 0; i < length; i++) {
     password += chars.charAt(Math.floor(Math.random() * chars.length));
