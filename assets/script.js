1ebe3b88e439baa49bc477090ccedbcba5935b94
Andrew Min <58195643+min-andrew@users.noreply.github.com>
2022-08-26 00:05:08 -0700

    worked on finishing touches worked on finishing touches

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
 // Assignment code here
 //
-
-var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numChar = "0123456789";
+var specialChar = "!@#$%^&*()";
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var totalChars = "";
 
 function generatePassword() {
   var length = prompt("how long do you want your password? (8-128)")
@@ -18,17 +22,97 @@ function generatePassword() {
       break
     }
   };
+
 // if you cancel out of the prompt, you dont want these to continue. Make a if statment for that
-  var upper = confirm("Do you want uppercase characters?");
-  var lower = confirm("Do you want lowercase characters?");
-  var special = confirm("Do you want special characters?");
-  var num = confirm("Do you want numbers?");
+  while (length !== null) {
+    var upper = confirm("Do you want uppercase characters?");
+    var lower = confirm("Do you want lowercase characters?");
+    var special = confirm("Do you want special characters?");
+    var num = confirm("Do you want numbers?");
 
-
+  // if true add to the addition of the total chars. if false, than take it out. 
+    if (upper === true){
+      if (lower === true){
+        if (special === true){
+          if (num === true){
+            totalChars = upperChar + lowerChar + specialChar + numChar;
+            break;
+          } else if (num === false){
+            totalChars = upperChar + lowerChar + specialChar;
+            break;
+          }
+        } else if (special === false) {
+          if (num === true){
+            totalChars = upperChar + lowerChar + numChar;
+            break;
+          } else if (num === false){
+            totalChars = upperChar + lowerChar;
+            break;
+          }
+        }
+      } else if (lower === false){
+        if (special === true){
+          if (num === true){
+            totalChars = upperChar + specialChar + numChar;
+            break;
+          } else if (num === false){
+            totalChars = upperChar + specialChar;
+            break;
+          }
+        } else if (special === false) {
+          if (num === true){
+            totalChars = upperChar + numChar;
+            break;
+          } else if (num === false){
+            totalChars = upperChar;
+            break;
+          }
+        }
+      }
+    } else if (upper === false){
+      if (lower === true){
+        if (special === true){
+          if (num === true){
+            totalChars = lowerChar + specialChar + numChar;
+            break;
+          } else if (num === false){
+            totalChars = lowerChar + specialChar;
+            break;
+          }
+        } else if (special === false) {
+          if (num === true){
+            totalChars = lowerChar + numChar;
+            break;
+          } else if (num === false){
+            totalChars = lowerChar;
+            break;
+          }
+        }
+      } else if (lower === false){
+        if (special === true){
+          if (num === true){
+            totalChars = specialChar + numChar;
+            break;
+          } else if (num === false){
+            totalChars = specialChar;
+            break;
+          }
+        } else if (special === false) {
+          if (num === true){
+            totalChars = numChar;
+            break;
+          } else if (num === false){
+            alert("You need to select at least one element to include in your password.");
+            generatePassword();
+          }
+        }
+      }
+    }
+  }
 
   var password = "";
   for (i = 0; i < length; i++) {
-    password += chars.charAt(Math.floor(Math.random() * chars.length));
+    password += totalChars.charAt(Math.floor(Math.random() * totalChars.length));
    }
    return password;
 }
